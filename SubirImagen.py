7d16c26dacaa742764895b403ae920c71ab1bf7d
yancefranco <105954764+yancefranco@users.noreply.github.com>
2024-10-30 16:57:59 -0500

    Update SubirImagen.py

--- SubirImagen.py
+++ SubirImagen.py
@@ -104,4 +104,6 @@ def detect_and_identify():
         return jsonify({"error": f"Error inesperado: {e}"}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
